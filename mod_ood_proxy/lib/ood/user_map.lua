00ae158d7908c0bf7673062dc7dcb20638b25857
Stephen Jones <stephen@bluerocksoft.com>
2026-02-24 18:27:57 +0000

    feat: pass OIDC_SUB env var to user_map_cmd
    Read OIDC_CLAIM_sub from Apache subprocess environment
    and prepend it as OIDC_SUB to the io.popen() command
    string so user_map.py can use the stable Keycloak sub
    for identity resolution.

--- mod_ood_proxy/lib/ood/user_map.lua
+++ mod_ood_proxy/lib/ood/user_map.lua
@@ -44,7 +44,9 @@ function map(r, user_map_match, user_map_cmd, remote_user)
     sys_user = string.match(remote_user, user_map_match)
   -- run user_map_cmd and read in stdout
   elseif user_map_cmd ~= nil then
-    local handle = io.popen(user_map_cmd .. " '" .. r:escape(remote_user) .. "'")
+    local oidc_sub = r.subprocess_env['OIDC_CLAIM_sub'] or ""
+    local cmd = "OIDC_SUB='" .. r:escape(oidc_sub) .. "' " .. user_map_cmd .. " '" .. r:escape(remote_user) .. "'"
+    local handle = io.popen(cmd)
     sys_user = handle:read()
     handle:close()
   end
